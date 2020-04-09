ec09f6ff0b707c6098f2b0194eb492abcecd8d03
YangChuan99 <1423880080@qq.com>
2020-04-09 21:18:05 +0800

    提交代码

---
+++ 20200409/TestOne.java
@@ -0,0 +1,15 @@
+public class TestOne {
+	public static void main(String[] args) {
+	
+		int num = 5;
+        int i = 1;
+        int ret = 1;
+        int sum = 0;
+        while(i <= num){
+        	ret *= i;
+        	sum += ret;
+        	i++;
+        }
+        System.out.println(sum);
+	}
+}
