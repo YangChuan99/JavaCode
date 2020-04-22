43f25f386505fd6baca7e4fa5c7fd27825a87986
YangChuan99 <1423880080@qq.com>
2020-04-22 10:30:56 +0800

    提交代码

---
+++ 20200422/src/Test0422.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+public class Test0422 {
+    public static void swap(int[] array, int[] array2) {
+        int tmp = 0;
+        for (int i = 0; i < array.length; i++) {
+            tmp = array[i];
+            array[i] = array2[i];
+            array2[i] = tmp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {0,1,2,3,4};
+        int[] array2= {5,6,7,8,9};
+        swap(array,array2);
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(array2));
+    }
+}
